9ee2b96e587856902a11666e6082d59818e1f656
BenQue <benque@gmail.com>
2025-08-19 22:29:56 +0800

    deploy: add domestic mirror sources to all Dockerfiles for faster Alpine package downloads

--- packages/database/dist/index.js
+++ packages/database/dist/index.js
@@ -14,7 +14,7 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
+exports.NotificationType = exports.FaultCode = exports.Priority = exports.WorkOrderStatus = exports.UserRole = exports.prisma = void 0;
 const client_1 = require("@prisma/client");
 const globalForPrisma = globalThis;
 exports.prisma = globalForPrisma.prisma ??
@@ -23,5 +23,46 @@ exports.prisma = globalForPrisma.prisma ??
     });
 if (process.env.NODE_ENV !== 'production')
     globalForPrisma.prisma = exports.prisma;
+// Export Prisma client types
 __exportStar(require("@prisma/client"), exports);
+var UserRole;
+(function (UserRole) {
+    UserRole["EMPLOYEE"] = "EMPLOYEE";
+    UserRole["TECHNICIAN"] = "TECHNICIAN";
+    UserRole["SUPERVISOR"] = "SUPERVISOR";
+    UserRole["ADMIN"] = "ADMIN";
+})(UserRole || (exports.UserRole = UserRole = {}));
+var WorkOrderStatus;
+(function (WorkOrderStatus) {
+    WorkOrderStatus["PENDING"] = "PENDING";
+    WorkOrderStatus["IN_PROGRESS"] = "IN_PROGRESS";
+    WorkOrderStatus["WAITING_PARTS"] = "WAITING_PARTS";
+    WorkOrderStatus["WAITING_EXTERNAL"] = "WAITING_EXTERNAL";
+    WorkOrderStatus["COMPLETED"] = "COMPLETED";
+    WorkOrderStatus["CANCELLED"] = "CANCELLED";
+})(WorkOrderStatus || (exports.WorkOrderStatus = WorkOrderStatus = {}));
+var Priority;
+(function (Priority) {
+    Priority["LOW"] = "LOW";
+    Priority["MEDIUM"] = "MEDIUM";
+    Priority["HIGH"] = "HIGH";
+    Priority["URGENT"] = "URGENT";
+})(Priority || (exports.Priority = Priority = {}));
+var FaultCode;
+(function (FaultCode) {
+    FaultCode["MECHANICAL_FAILURE"] = "MECHANICAL_FAILURE";
+    FaultCode["ELECTRICAL_FAILURE"] = "ELECTRICAL_FAILURE";
+    FaultCode["SOFTWARE_ISSUE"] = "SOFTWARE_ISSUE";
+    FaultCode["WEAR_AND_TEAR"] = "WEAR_AND_TEAR";
+    FaultCode["USER_ERROR"] = "USER_ERROR";
+    FaultCode["PREVENTIVE_MAINTENANCE"] = "PREVENTIVE_MAINTENANCE";
+    FaultCode["EXTERNAL_FACTOR"] = "EXTERNAL_FACTOR";
+    FaultCode["OTHER"] = "OTHER";
+})(FaultCode || (exports.FaultCode = FaultCode = {}));
+var NotificationType;
+(function (NotificationType) {
+    NotificationType["WORK_ORDER_ASSIGNED"] = "WORK_ORDER_ASSIGNED";
+    NotificationType["WORK_ORDER_UPDATED"] = "WORK_ORDER_UPDATED";
+    NotificationType["SYSTEM_ALERT"] = "SYSTEM_ALERT";
+})(NotificationType || (exports.NotificationType = NotificationType = {}));
 exports.default = exports.prisma;
